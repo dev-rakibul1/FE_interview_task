cde588b2b9037923e51862fa19a5e7603048f87f
Your Name <you@example.com>
2023-07-05 00:07:30 +0600

    [DONE] hero bg and so on

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
+const bgImage = "https://i.ibb.co/5v51vZW/hero-bg.png";
+
 const Home = () => {
   return (
-    <div className="theme-width">
+    <div
+      className="theme-width w-full h-full py-24"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
       {/* <HeroTopBar /> */}
       {/* <HeroLogoArea /> */}
     </div>
--- src/shared/navbar/Navbar.jsx
+++ src/shared/navbar/Navbar.jsx
@@ -1,77 +1,83 @@
 import React from "react";
+import { MdFormatListBulleted } from "react-icons/md";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const navItems = (
+    <>
+      <li className="navbar-items-hover bg-transparent ">
+        <Link
+          to="/"
+          className="navbar-link focus:bg-transparent hover:bg-transparent"
+        >
+          Hotels
+        </Link>
+      </li>
+
+      <li className="navbar-items-hover bg-transparent ">
+        <Link
+          to="/"
+          className="navbar-link focus:bg-transparent hover:bg-transparent"
+        >
+          Flights
+        </Link>
+      </li>
+      <li className="navbar-items-hover bg-transparent ">
+        <Link
+          to="/"
+          className="navbar-link focus:bg-transparent hover:bg-transparent"
+        >
+          Flights + Hotels
+        </Link>
+      </li>
+      <li className="navbar-items-hover bg-transparent ">
+        <Link
+          to="/"
+          className="navbar-link focus:bg-transparent hover:bg-transparent"
+        >
+          Cars
+        </Link>
+      </li>
+      <li className="navbar-items-hover bg-transparent ">
+        <Link
+          to="/"
+          className="navbar-link focus:bg-transparent hover:bg-transparent"
+        >
+          Activities
+        </Link>
+      </li>
+      <li className="navbar-items-hover bg-transparent ">
+        <Link
+          to="/"
+          className="navbar-link focus:bg-transparent hover:bg-transparent"
+        >
+          Tours
+        </Link>
+      </li>
+    </>
+  );
+
   return (
-    <div className="theme-width">
-      <div className="navbar  bg-white">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+    <div className="bg-gray-100 text-gray-400">
+      <div className="theme-width py-7">
+        <div className="navbar mr-auto">
+          <div className="">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <MdFormatListBulleted className="text-2xl" />
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
+                {navItems}
+              </ul>
+            </div>
           </div>
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li tabIndex={0}>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">{navItems}</ul>
+          </div>
         </div>
       </div>
     </div>
