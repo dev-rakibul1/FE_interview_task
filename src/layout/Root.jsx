744316474f53c2cd58e14679071dae6adc5ea061
Your Name <you@example.com>
2023-07-04 22:53:19 +0600

    [DONE]hero top bar and logo area

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import HeroLogoArea from "../components/heroLogoArea/HeroLogoArea";
+import HeroTopBar from "../components/heroTopBar/HeroTopBar";
 import Navbar from "../shared/navbar/Navbar";
 
 const Root = () => {
   return (
     <div>
+      <HeroTopBar />
+      <HeroLogoArea />
       <Navbar />
       <Outlet />
     </div>
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="theme-width">
+      {/* <HeroTopBar /> */}
+      {/* <HeroLogoArea /> */}
+    </div>
+  );
+};
+
+export default Home;
