662bde44f4f13b30447b443959a0ba4ab27895a3
Your Name <you@example.com>
2023-07-05 17:13:01 +0600

    Navbar active doen

---
+++ src/pages/cars/Cars.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cars = () => {
+  return (
+    <div className="theme-container h-screen flex items-center justify-center">
+      <h1 className="font-bold text-4xl">Cars page is coming soon.</h1>
+    </div>
+  );
+};
+
+export default Cars;
---
+++ src/pages/tours/Tours.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Tours = () => {
+  return (
+    <div className="theme-container h-screen flex items-center justify-center">
+      <h1 className="font-bold text-4xl">Tours page is coming soon.</h1>
+    </div>
+  );
+};
+
+export default Tours;
