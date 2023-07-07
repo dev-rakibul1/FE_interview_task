decdedcb8ea3edade399d4979d945715308d49ea
Your Name <you@example.com>
2023-07-07 07:14:15 +0600

    Search almost done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import '@syncfusion/ej2-react-calendars/styles/material.css';
 import React from 'react';
+import "react-datepicker/dist/react-datepicker.css";
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
